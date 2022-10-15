df7a3657967f72c57b412b7066d9f8d1b136c924
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-10-15 23:28:46 +0530

    Time: 9 ms (58.18%), Space: 9 MB (23.23%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -4,44 +4,33 @@ public:
         char prev =  chars[0];
         int count = 1;
         vector<char> ans ;
+        int n = chars.size();
         ans.push_back(chars[0]);        // set all default value acc to first char  already choosen
         bool flag = false;
-        for(int i =1;i<chars.size();i++){
-            char ch = chars[i];
-            if(ch == prev) {        // keep counting 
-                count++;
-            }
-            else {
+        for(int i =1;i<=n;i++){
+            char ch = i == n ? ' ' : chars[i];
+            if(ch!= prev || i == n) {
                 if(count > 1){
-                    string s = "";
+                    string s = "";          // storing count in string and then putting it into vector
                     while(count>0){
                         int l = count%10;
                         s.push_back(l + '0');
                         count /= 10;
                     }
                     for(int i = s.size()-1;i>=0;i--){
-                        // cout<<s[i]<<' ';
                         ans.push_back(s[i]);
                     }
-                    // cout<<endl;
                 }
                 count = 1;
                 prev = ch;
                 ans.push_back(ch);
             }
+            else {              // keep counting
+                count++;
+            }
         }
+        ans.pop_back();
         chars.clear();
-        if(count>1) {
-            string s = "";
-            while(count>0){
-                int l = count%10;
-                s.push_back(l + '0');
-                    count /= 10;
-            }
-            for(int i = s.size()-1;i>=0;i--){
-                ans.push_back(s[i]);
-            }
-        }
         for(auto i : ans) {
             chars.push_back(i);
         }       // pushing updated values in chars vector
