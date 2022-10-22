4cf1c28d7808581c2fd90af9c637ab354703e19f
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-10-22 19:36:05 +0530

    Time: 448 ms (23.07%), Space: 65.7 MB (33.30%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -3,32 +3,16 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int ans = 0;
-        map<int,int> m;
-        int next = 0;
+        int next = 0;               // it will keep track of next number we can insert
         for(int i = 0;i<nums.size();i++){
             int val = nums[i];
-            if(m.find(val) == m.end()){
-                m[val] = 1;
-                if(next>val){
-                    ans = ans + (next-val);
-                    next = next+1;
-                }
-                else {
-                    next = val+1;
-                }
+            if(next>val){       // 
+                ans = ans + (next-val);
+                next = next+1;
             }
             else {
-                if(next>val){
-                    ans = ans + (next-val);
-                    next = next+1;
-                }
-                else {
-                    next = val+1;
-                }
+                next = val+1;
             }
-            // for(auto i : m) cout<<i.first<<' '<<i.second<<endl;
-            // cout<<ans<<' '<<next<<' '<<endl;
-            // cout<<endl;
         }
         return ans;
     }
