0ce694ba45488a7898d01a85d868b3fec2f5b909
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-10-22 19:37:28 +0530

    Time: 427 ms (27.62%), Space: 65.7 MB (33.30%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -6,11 +6,12 @@ public:
         int next = 0;               // it will keep track of next number we can insert
         for(int i = 0;i<nums.size();i++){
             int val = nums[i];
-            if(next>val){       // 
+            if(next>val){       // if next number we can insert is greater than current than 
+                // we have to inc current number by next - val times
                 ans = ans + (next-val);
                 next = next+1;
             }
-            else {
+            else {              // and if it is less than or equal to current element than we don't have to increment it just set next number we can insert as current val +1
                 next = val+1;
             }
         }
