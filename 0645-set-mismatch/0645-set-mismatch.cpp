082bb0aca78842eb0cac63ef17a12cc98bfe2587
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-10-27 03:45:14 +0530

    Time: 215 ms (5.05%), Space: 33 MB (7.28%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int p = 0, acc1 = 0, acc2 = 0;
-        // Get the xor of missing and duplicate numbers
-        for (unsigned i = 0; i < nums.size(); ++i)
-            p ^= (i + 1) ^ nums[i];
-
-        p &= -p; // We'll use only the last significant set bit
-
-        // Split the numbers in 2 categories and xor them
-        for (unsigned i = 0; i < nums.size(); ++i)
-        {
-            ((nums[i] & p) == 0) ? acc1 ^= nums[i] : acc2 ^= nums[i];
-            (((i + 1) & p) == 0) ? acc1 ^= i + 1 : acc2 ^= i + 1;
+        map<int,int> m;
+        int n = nums.size();
+        for(auto i : nums) m[i]++;
+        vector<int> ans(2,0);
+        for(int i = 1;i<=n;i++){
+            if(m[i] == 2) ans[0] = i;
+            if(m[i] == 0) ans[1] = i;
+            // if(ans[0] !=0  && ans[1] !=0 ) return ans;
         }
-
-        // Determine which is the duplicate number
-        for (auto n : nums)
-            if (n == acc1)
-                return {acc1, acc2};
-        return {acc2, acc1};
+        return ans;
     }
 };
