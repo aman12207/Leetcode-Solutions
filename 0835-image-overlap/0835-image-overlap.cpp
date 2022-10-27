b3acf6bfb1665ea5a37f39e0a614e7b2108b2ebb
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-10-27 20:15:36 +0530

    Time: 173 ms (51.39%), Space: 9.4 MB (77.78%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int ans = 0;
+        int row = img1.size(),col = img1[0].size();
+        for(int i =0;i<row;i++){
+            for(int j = 0;j<col;j++){     // trying to shift all the places
+                int count1 = 0,count2 = 0,count3 = 0,count4 = 0;
+                for(int r = 0;r+i<row;r++){
+                    for(int c = 0;c+j<col;c++){
+                        // cout<<r+i<<c+j<<' ';
+                        if(img1[r+i][c+j] == 1 && img2[r][c] == 1){     // right down
+                            // cout<<(r+l)<<' '<<r<< " " << (c+j)<<' '<<c<<endl;
+                            count1++;
+                        }
+                        if(img2[r+i][c+j] == 1 && img1[r][c] == 1){     // left up
+                            // cout<<(r+l)<<' '<<r<< " " << (c+j)<<' '<<c<<endl;
+                            count2++;
+                        }
+                    }
+                    // cout<<endl;
+                }
+                // cout<<endl;
+                for(int r = i;r<row;r++){
+                    for(int c = 0;c+j<col;c++){
+                        // cout<<r+i<<c+j<<' ';
+                        if(img1[r-i][c+j] == 1 && img2[r][c] == 1){     // shifing in up right
+                            // cout<<(r+l)<<' '<<r<< " " << (c+j)<<' '<<c<<endl;
+                            count3++;
+                        }
+                        if(img2[r-i][c+j] == 1 && img1[r][c] == 1){    // shifing in bottom left direction 
+                            // cout<<(r+l)<<' '<<r<< " " << (c+j)<<' '<<c<<endl;
+                            count4++;
+                        }
+                    }
+                    // cout<<endl;
+                }
+                // for(int r = row-1;r-i>=0;r--){
+                //     for(int c = col-1;c-j>=0;c--){
+                //         if(img1[r-i][c-j] == 1 && img2[r][c] == 1){
+                //             // cout<<(r+l)<<' '<<r<< " " << (c+j)<<' '<<c<<endl;
+                //             count2++;
+                //         }
+                //     }
+                // }
+                // cout<<endl;
+                ans = max(ans,max(count1,max(count2,max(count3,count4))));
+            }
+        }
+        return ans;
+    }
+};
