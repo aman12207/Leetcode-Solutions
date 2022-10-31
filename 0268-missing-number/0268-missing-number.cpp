d9d19e4b96815734f6e1648e0de74c7bbc7cfef7
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-01 04:02:31 +0530

    Time: 23 ms (88.18%), Space: 18.1 MB (20.80%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // cyclic sort
+        int i =0;
         int n = nums.size();
+        while(i<n){
+            if(nums[i] != n && i != nums[i]){
+                swap(nums[i],nums[nums[i]]);
+            }
+            else i++;
+        }
         for(int i = 0;i<n;i++){
             if(nums[i] != i){
                 return i;
