ae44f1fefcbfac29d86f69485103621961df3245
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-01 04:05:32 +0530

    Time: 50 ms (39.36%), Space: 18.1 MB (20.80%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -5,7 +5,11 @@ public:
         int i =0;
         int n = nums.size();
         while(i<n){
-            if(nums[i] != n && i != nums[i]){
+            if(nums[i] != n && i != nums[i]){       // it will keep swapping the element 
+                // like [3,0,1] at 0 
+                // it will be [0,3,1] and i remains at 1 index
+                // as it will 3 then i will be inc
+                // 3 will be swapped with 1 and then again i will inc and loop will break;
                 swap(nums[i],nums[nums[i]]);
             }
             else i++;
