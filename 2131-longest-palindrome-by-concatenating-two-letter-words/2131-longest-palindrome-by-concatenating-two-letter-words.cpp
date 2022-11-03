f922726534868939dd59022f1c743a1cbc4e1dcb
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-04 02:25:48 +0530

    Time: 1479 ms (9.04%), Space: 168 MB (60.57%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,37 +2,21 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         map<string, int > m;
-        for(auto i : words){
-            m[i]++;
-        }
-        int ans = 0,ispalindromeAdded = 0;// we can add a palindrome string middle of a palindrome string
-        for(auto &i : m){
-            string rev = i.first;
+        int ans = 0;
+        for(auto &i : words){           // just traverse each string if its rev is found dec the count from map and inc the ans    
+            string rev = i;
             reverse(rev.begin(),rev.end());
-            if(i.first == rev){
-                if(i.second>1){
-                    int curr = i.second;
-                    i.second = curr%2;
-                    ans += ((curr/2)*4);        // no of string added will be (curr/2) * 2 and no of char will be *2 so we have to add (curr/2)*4 to ans
-                    
-                }
-                if(!ispalindromeAdded && i.second == 1){
-                        ans+=2;
-                        ispalindromeAdded = 1;
-                        i.second = 0;           // there is still one palindromic string remains and we haven't added any in the middle then add this one and set the flag to true;
-                    }
+            if(m[rev]){
+                m[rev]--;
+                ans+=4;
             }
-            else if(i.second > 0){
-                if(m.find(rev) != m.end()){
-                    int curr = min(m[rev], i.second);
-                    if(m[rev] >0){
-                        ans += (curr*4);        // if rev is also availabe in the map then add as much as possible i.e min of both the available 
-                        m[rev]-=curr;
-                        m[i.first]-=curr;
-                    }
-                }
+            else m[i]++;
+        }
+        for(auto i : m){        // we can add a palindromic string with count 1 in the middle of the final string 
+            if(i.first[0] == i.first[1] && i.second == 1) {
+                ans+=2;
+                break;
             }
-            // cout<<i.first<<' '<<i.second<<endl;
         }
         return ans;
     }
