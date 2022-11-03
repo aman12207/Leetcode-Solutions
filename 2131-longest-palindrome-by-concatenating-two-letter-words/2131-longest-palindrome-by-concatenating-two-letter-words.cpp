a41ccf8d0e89c907cb9bd05434df535be43fff6b
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-04 02:12:06 +0530

    Time: 1019 ms (28.62%), Space: 167.9 MB (60.57%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -13,25 +13,20 @@ public:
                 if(i.second>1){
                     int curr = i.second;
                     i.second = curr%2;
-                    ans += ((curr/2)*4);
-                    if(!ispalindromeAdded && i.second == 1){
+                    ans += ((curr/2)*4);        // no of string added will be (curr/2) * 2 and no of char will be *2 so we have to add (curr/2)*4 to ans
+                    
+                }
+                if(!ispalindromeAdded && i.second == 1){
                         ans+=2;
                         ispalindromeAdded = 1;
-                        i.second = 0;
+                        i.second = 0;           // there is still one palindromic string remains and we haven't added any in the middle then add this one and set the flag to true;
                     }
-                }
-                else if(!ispalindromeAdded && i.second == 1){
-                    // cout<<i.first<<' '<<i.second<<endl;
-                    ans+=2;
-                    ispalindromeAdded = 1;
-                    i.second = 0;
-                }
             }
             else if(i.second > 0){
                 if(m.find(rev) != m.end()){
                     int curr = min(m[rev], i.second);
                     if(m[rev] >0){
-                        ans += (curr*4);
+                        ans += (curr*4);        // if rev is also availabe in the map then add as much as possible i.e min of both the available 
                         m[rev]-=curr;
                         m[i.first]-=curr;
                     }
