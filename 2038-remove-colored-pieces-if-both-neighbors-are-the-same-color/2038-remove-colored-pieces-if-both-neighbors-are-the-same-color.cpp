f7ec380042475ca51edf7acbfcbaa412b4e951a4
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-10 02:12:52 +0530

    Time: 183 ms (5.09%), Space: 13.2 MB (93.67%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
+        // just count the occurence of "AAA" and "BBB" as we are deleting only middle one AA and BB will still left so it will not inc the chance for other so one with maxm availablity will win
         int count_alice = 0 , count_bob = 0;
         int n = colors.size();
         for(int i = 0;i<n-2;i++){
@@ -8,7 +9,7 @@ public:
             if(s == "AAA" ) count_alice++;
             if(s == "BBB") count_bob++;
         }
-        cout<<count_alice<<' '<<count_bob<<endl;
+        // cout<<count_alice<<' '<<count_bob<<endl;
         return count_alice>count_bob;
     }
 };
