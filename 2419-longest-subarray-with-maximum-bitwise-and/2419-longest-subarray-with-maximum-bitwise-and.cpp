d49eff0faed8526cc8431a84454e629dece6e102
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-10 01:42:28 +0530

    Time: 523 ms (5.04%), Space: 82.3 MB (14.78%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int maxm = 0;
-        for(auto i : nums){
+        for(auto i : nums){         // just search the maxm no
             maxm = max(i,maxm);
         }
         int ans = 0, count = 0, i = 0;
-        while(i<nums.size()){
+        while(i<nums.size()){       // and longest subarray with consecutive maxm element 
             if(nums[i] == maxm){
                 while(i<nums.size() && nums[i] == maxm ){
                     i++;
