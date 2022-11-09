800b046465d30af9be940dbbfdd186e2ec4c72a9
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-10 01:59:14 +0530

    Time: 437 ms (24.85%), Space: 98.7 MB (24.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int,int>> st;
+        stack<pair<int,int>> st;        //{val,index}
         int n = temperatures.size();
         vector<int> ans(n,0);
-        for(int i = n-1;i>=0;i--){
+        for(int i = n-1;i>=0;i--){          // just find nearest greater to the right
             while(!st.empty() && st.top().first<=temperatures[i]){
                 st.pop();
             }
             if(!st.empty())
-                ans[i] = st.top().second-i;
-            st.push({temperatures[i],i});
+                ans[i] = st.top().second-i;     // insert the diff in index in ans
+            st.push({temperatures[i],i});       
         }
         return ans;
     }
