5af43df9b2bb8ce67c96123da2c49a044c9cbdc7
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-14 01:30:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (60.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int solve(int x, int y,vector<vector<int>> &dp){      // actually we are doing the opp we are trying to reach 0 0  from m-1, n-1
-        if(x== 0 && y == 0){                // found ans 
-            return 1;
-        }
-        if(x<0 || y<0)         // base cases
-            return 0;
-        if(dp[x][y] != -1) return dp[x][y];
-        int top = solve(x-1,y,dp);
-        int left = solve(x,y-1,dp);        // calls
-        return dp[x][y] = top + left;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        vector<int> dp(n,1);
+        for(int i = 1;i<m;i++){
+            vector<int> temp(n,-1);
+            temp [0] = 1;           // for first col there is only 1 way to reach it
+            for(int j = 1;j<n;j++){
+                int top = 0, left = 0;
+                top = dp[j];           // dp array is keepint track of prev row
+                left = temp[j-1];       
+                temp[j] = top + left;
+            }
+            dp = temp;
+        }
+        return dp[n-1];
     }
 };
