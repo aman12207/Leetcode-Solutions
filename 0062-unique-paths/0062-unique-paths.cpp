fd7b54dd27c36f0423f62cf03bed9a875486f260
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-14 01:06:55 +0530

    Time: 3 ms (46.26%), Space: 6.7 MB (29.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &visited, int x, int y,vector<vector<int>> &dp){      // actually we are doing the opp we are trying to reach 0 0  from m-1, n-1
-        if(x== 0 && y == 0){                // found ans 
-            return 1;
-        }
-        if(x<0 || y<0)         // base cases
-            return 0;
-        if(visited[x][y]) return 0;
-        if(dp[x][y] != -1) return dp[x][y];
-        visited[x][y] = 1;              // marking
-        int top = solve(visited,x-1,y,dp);
-        int left = solve(visited,x,y-1,dp);        // calls
-        visited[x][y] = 0;              // backtracking
-        return dp[x][y] = top + left;
-    }
     int uniquePaths(int m, int n) {
         vector<vector<int>> visited(m, vector<int>(n,0));
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(visited,m-1,n-1,dp);
+        for(int i = 0;i<n;i++){
+            dp[0][i] = 1;
+        }
+        for(int i = 0;i<m;i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
