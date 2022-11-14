2db2b16b59a1cc65b74dd49054b67701ca22242b
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-14 10:25:49 +0530

    Time: 4 ms (74.31%), Space: 7.8 MB (69.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n,0));
+        vector<int> dp(n,0);        // it keeps track of prev row
         for(int i = 0;i<m;i++){
+            vector<int> curr(n,0);      // it keeps track of curr row
             for(int j = 0;j<n;j++){
                 if(obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 }         // if it is an obstacle mark it as 0 as there is no way to reach that block
                 // if grid is 0 1 0 0 as we are adding top and left 
                 // the dp will be 1 0 0 0 a
-                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else if(i == 0 && j == 0) curr[j] = 1;
                 else {
                     int top = 0, left = 0;
-                    if(i>0) top = dp[i-1][j];
-                    if(j>0 ) left = dp[i][j-1];
-                    dp[i][j] = top + left;
+                    if(i>0) top = dp[j];
+                    if(j>0 ) left = curr[j-1];
+                    curr[j] = top + left;
                 }
             }
+            dp = curr;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
