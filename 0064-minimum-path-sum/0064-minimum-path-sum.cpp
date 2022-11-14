9f22179ba2ae871150fb286250a325bc0216082a
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-14 10:29:51 +0530

    Time: 23 ms (39.07%), Space: 10 MB (67.10%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
+        // just replace dp[n-1][j] with dp[j] as dp is storing prev row
+        // dp[n][j] with curr [j] as it is keeping track of curr row
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> dp(n,0);
         for(int i = 0;i<m;i++){
+            vector<int> curr(n,0);
             for(int j = 0;j<n;j++){
-                if(i == 0 && j == 0) {
-                    dp[0][0] = grid[0][0];
+                if(i == 0 && j == 0) {      // base case
+                    curr[j] = grid[0][0];
                     continue;
                 }
-                int top = INT_MAX, left = INT_MAX;
-                if(i>0) top = dp[i-1][j];
-                if(j>0) left = dp[i][j-1];
-                dp[i][j] = min(top , left) + grid[i][j];
+                int top = INT_MAX, left = INT_MAX;          // if one of them doesn't exist we have to chooose other
+                if(i>0) top = dp[j];
+                if(j>0) left = curr[j-1];
+                curr[j] = min(top , left) + grid[i][j];
             }
+            dp = curr;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
