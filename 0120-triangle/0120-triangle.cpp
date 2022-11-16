91e7bb16003c8d6ec577b375f36441d0fe3e6b5d
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-16 08:53:50 +0530

    Time: 14 ms (47.36%), Space: 9 MB (52.20%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
-        vector<vector<int>> dp;
-        for(int i = 0;i<m;i++){         // creating dp array of same size as triangle
-            dp.push_back(vector<int>(i+1, -1));
-        }
+        vector<int> prev(m,-1);
         for(int i = 0;i<m;i++){
+            vector<int> curr (i+1, -1);           // each row have same no of col as row no
             for(int j = 0;j<=i;j++){
                 if(i == 0 && j == 0){           // handling base case
-                    dp[i][j] = triangle[0][0];
+                    curr[j] = triangle[0][0];
                     continue;
                 }
                 int topLeft = INT_MAX, top = INT_MAX;
-                if(i>0 && j>0) topLeft = dp[i-1][j-1];          // we can choose from topleft and left
-                if(j != i) top = dp[i-1][j];
-                dp[i][j] = min(top,topLeft) + triangle[i][j];
+                if(i>0 && j>0) topLeft = prev[j-1];          // we can choose from topleft and left
+                if(j != i) top = prev[j];
+                curr[j] = min(top,topLeft) + triangle[i][j];
             }
+            prev = curr;
         }
         int ans = INT_MAX;
-        for(auto i : dp[m-1]){          // any of the last row might be our answer so we have to check for all of them
+        for(auto i : prev){          // any of the last row might be our answer so we have to check for all of them
             ans = min(ans,i);
         }
         return ans;
