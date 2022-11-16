fb6e5a3c837ba7df879feb103e56b3646cbc694c
Aman Gupta <88676116+aman12207@users.noreply.github.com>
2022-11-16 09:56:35 +0530

    Time: 37 ms (37.58%), Space: 10.2 MB (66.55%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m,vector<int> (n,-1));
+        vector<int> prev(n,-1);
         int ans =  INT_MAX;
         for(int i = 0;i<m;i++){
+            vector<int> curr(n,-1);
             for(int j = 0 ;j<n;j++){
                 if(i == 0 ){                // base case
-                    dp[i][j] = matrix[i][j] ;
+                    curr[j] = matrix[i][j] ;
                     continue;
                 }
                 int top = INT_MAX, topleft = INT_MAX, topright = INT_MAX;
-                top = dp[i-1][j];
-                if(j>0 )topleft = dp[i-1][j-1];        // calls
-                if(j<m-1) topright = dp[i-1][j+1];
-                dp[i][j] = min(top, min(topleft,topright)) + matrix[i][j];
+                top = prev[j];
+                if(j>0 )topleft = prev[j-1];        // calls
+                if(j<m-1) topright = prev[j+1];
+                curr[j] = min(top, min(topleft,topright)) + matrix[i][j];
             }
+            prev = curr;
         }
-        for(auto i : dp[m-1]){          // find the minm possilbe ans
+        for(auto i : prev){          // find the minm possilbe ans
             ans = min(ans,i);
         }
         return ans;
